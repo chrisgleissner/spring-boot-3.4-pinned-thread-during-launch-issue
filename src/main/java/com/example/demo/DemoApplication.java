1ff4d0f5dc6d9571e955744afdbc91f4b9200a77
Christian Gleissner <chrisgleissner@gmail.com>
2024-11-25 07:22:49 +0000

    Graceful shutdown

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,7 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args).close();
+
+	public static void main(String[] args) throws InterruptedException {
+		var context = SpringApplication.run(DemoApplication.class, args);
+		Thread.sleep(3000);
+		context.close();
 	}
 }
